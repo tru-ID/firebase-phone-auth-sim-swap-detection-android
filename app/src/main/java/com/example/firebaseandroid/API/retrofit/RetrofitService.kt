1f9c93e4d7f90f6e4fddf03d64ca0d5b3036664e
glamboyosa <ogbemudiatimothy@gmail.com>
2021-05-25 14:27:17 +0100

    feat: initial setup

---
+++ app/src/main/java/com/example/firebaseandroid/API/retrofit/RetrofitService.kt
@@ -0,0 +1,20 @@
+package com.example.firebaseandroid.API.retrofit
+
+import com.example.firebaseandroid.API.data.SIMCheckPost
+import com.example.firebaseandroid.API.data.SIMCheckResult
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface RetrofitService {
+    @Headers("Content-Type: application/json")
+    @POST("/phone-check")
+    suspend fun createPhoneCheck(@Body user: SIMCheckPost): Response<SIMCheckResult>
+
+    companion object {
+        // set up base_url in the format https://{subdomain}.loca.lt gotten from localTunnel URL
+        const val base_url = "https://tru-id.loca.lt"
+    }
+
+}
