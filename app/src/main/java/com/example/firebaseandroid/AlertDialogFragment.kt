00b748db518edb25ed1d054a4e08856e6f165466
glamboyosa <ogbemudiatimothy@gmail.com>
2021-05-26 11:31:07 +0100

    feat: created dialog. created form UI

---
+++ app/src/main/java/com/example/firebaseandroid/AlertDialogFragment.kt
@@ -0,0 +1,28 @@
+package com.example.firebaseandroid
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class AlertDialogFragment(val message: String) : DialogFragment(){
+      override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage(message)
+                .setPositiveButton("Okay",
+                    DialogInterface.OnClickListener { dialog, id ->
+
+                    })
+                .setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                        getDialog()?.cancel()
+                    })
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
